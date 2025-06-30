7afac105eb1ed2402329adb4cd0bca2cc410a885
sec <violasangut@gmail.com>
2025-06-30 13:29:25 +0400

    Trigger Docker build

--- backend_main/src/server.js
+++ backend_main/src/server.js
@@ -21,7 +21,7 @@ app.use(responseFormatter); // Response Formatter
 app.get("/", (req, res) => {
   res.status(200).json({
     success: true,
-    message: "Welcome to the backend server",
+    message: "Welcome to the Hekopay backend server",
   });
 });
 
@@ -43,7 +43,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, async () => {
   try {
     await connectDatabase();
-    console.log(`SERVER IS RUNNING ON: http://localhost:${PORT}`);
+    console.log(`SERVER IS RUNNING ON: http://0.0.0.0:${PORT}`);
 
   } catch (error) {
     console.error("❌ Error starting the server:", error);
