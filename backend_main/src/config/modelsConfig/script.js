21aba6f4d169527faba5a70464956f830ef9dd1b
ritahchanger <peterdennis573@gmail.com>
2025-06-30 16:10:39 +0300

    Updates on the Audit wallet

--- backend_main/src/config/modelsConfig/script.js
+++ backend_main/src/config/modelsConfig/script.js
@@ -1,4 +1,6 @@
-const { Sequelize } = require("sequelize");
+const path = require("path")
+
+require("dotenv").config({ path: path.resolve(__dirname, "../../../.env") });
 
 
 const db = require("./index.js"); 
